0059674e47a593b78c2b7630bc101493d6a0a347
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2026-01-30 00:18:16 +0530

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    packages=find_packages(),
+    author = "Dinesh Tyagi",
+    author_email="dineshtyagi1511@gmail.com",
+    install_requires= []
+    
+)
