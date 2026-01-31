a3bdd2b73a2af1d1e582ddc42915c15b47e13885
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2026-01-31 21:57:56 +0530

    update

---
+++ src/mlProject/pipeline/prediction.py
@@ -0,0 +1,12 @@
+import joblib
+import numpy as np 
+from pathlib import Path 
+
+class PredictionPipeline:
+    def __init__(self):
+        self.model = joblib.load(Path("artificats/model_trainer/model.joblib"))
+
+    def prediction(self,data):
+        prediction = self.model.predict(data)    
+
+        return prediction
